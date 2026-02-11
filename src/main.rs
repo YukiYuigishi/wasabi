97ab36c43a653328769ba70c5ed4ac8c92c63fb3
Yuki Yuigishi <me@yuigishi.dev>
2026-02-11 23:57:42 +0900

    ページテーブルを一部実装

--- src/main.rs
+++ src/main.rs
@@ -32,7 +32,7 @@ fn efi_main(image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
     info!("info");
     warn!("warn");
     error!("error");
-    hexdump(efi_system_table);
+    //hexdump(efi_system_table);
     let mut vram = init_vram(efi_system_table).expect("init_vram failed");
     let vw = vram.width();
     let vh = vram.height();
@@ -57,8 +57,16 @@ fn efi_main(image_handle: EfiHandle, efi_system_table: &EfiSystemTable) {
     .unwrap();
     writeln!(w, "Hello, Non-UEFI world!").unwrap();
     let cr3 = wasabi::x86::read_cr3();
-    println!("cr3 = {cr3:#p}");
-    hexdump(unsafe { &*cr3 });
+    println!("cr3={cr3:#p}");
+    let t = Some(unsafe { &*cr3 });
+    println!("{t:?}");
+    let t = t.and_then(|t| t.next_level(0));
+    println!("{t:?}");
+    let t = t.and_then(|t| t.next_level(0));
+    println!("{t:?}");
+    let t = t.and_then(|t| t.next_level(0));
+    println!("{t:?}");
+
     loop {
         hlt();
     }
